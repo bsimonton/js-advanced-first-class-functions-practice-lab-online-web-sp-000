17ffc1179c79314be7952f1017603d0260f3ec6e
Brian Simonton <bsimonton78@gmail.com>
2019-07-08 00:53:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+
+const logDriverNames = function (drivers) {
+  
+  drivers.forEach(function (driver, i, arr) {
+    
+    console.log(driver.name);
+    
+  });
+};
+
+
+
+const logDriversByHometown = function (drivers, location) {
+  
+  drivers.forEach(function (driver, i, arr) {
+    
+    if (driver.hometown === location) {
+      
+      console.log(driver.name);
+      
+    }
+    
+  });
+};
+
+
